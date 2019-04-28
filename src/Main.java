d95d90627c028a3e4f1ceb009b3599a3fc4d6fe4
Roshan Prabhakar <roshan@aprabhakar.com>
2019-04-27 20:24:11 -0700

    note: increasing Cluster finder increases null pointer chance exponentially

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ClusterFinder finder = new ClusterFinder("logo.png");
-        finder.colorizeImage(2, true, Color.ORANGE, Color.WHITE);
-
-        ClusterFinder secondNode = new ClusterFinder(finder.getImage());
-        secondNode.colorizeImage(2, true, Color.BLACK, Color.RED);
-
-        ClusterFinder thirdNode = new ClusterFinder(finder.getImage());
-        secondNode.colorizeImage(2, true, Color.WHITE, Color.BLACK);
-
-        thirdNode.resizeImage(300);
-        thirdNode.writeToFile("outFile.png");
+        ClusterFinder finder = new ClusterFinder("outFile.png");
+        finder.colorizeImage(2, true, Color.BLACK, Color.BLACK);
+        finder.resizeImage(1000);
+        finder.writeToFile("outFile.png");
 
     }
 }
